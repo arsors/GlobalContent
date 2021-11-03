03565030a79fc5b4605bebd38af1d08e7ed96284
Marvin Schieler <mars@moon-agentur.de>
2021-11-03 12:54:16 +0100

    Add get EelHelper

--- Classes/EelHelper/GlobalContent.php
+++ Classes/EelHelper/GlobalContent.php
@@ -10,70 +10,112 @@
 
 namespace Arsors\GlobalContent\EelHelper;
 
+use Neos\Flow\Annotations as Flow;
+use Neos\Eel\FlowQuery\FlowQuery;
 use Neos\Eel\ProtectedContextAwareInterface;
 
-class GlobalContent implements ProtectedContextAwareInterface {
+class GlobalContent implements ProtectedContextAwareInterface
+{
 
-  /**
-   * Generate Global Content Array
-   *
-   * @param array $groups
-   * @param array $properties
-   * @return array
-   */
-  public function generateGlobalContentArray($groups, $properties)
-  {
-    // Set group array if no group set
-    if (!isset($groups)) $groups = [];
+    /**
+     * @Flow\InjectConfiguration(package="Arsors.GlobalContent")
+     * @var array
+     */
+    protected $configuration = array();
 
-    // Init variables
-    $defaultKey = 'default';
-    $defaultLabel = 'General';
-    $defaultCollapsed = false;
-    $defaultIcon = 'file';
-    $skipProperties = ['metaRobotsNoindex', 'metaRobotsNofollow', '_hiddenInIndex'];
+    /**
+     * @Flow\Inject
+     * @var Neos\ContentRepository\Domain\Service\ContextFactoryInterface
+     */
+    protected $contextFactory;
 
-    // Create group array and add properties
-    foreach($groups as $key => $value) {
-      $group = $key;
-      if (!isset($value['collapsed'])) $groups[$key]['collapsed'] = $defaultCollapsed;
-      if (!isset($value['icon'])) $groups[$key]['icon'] = $defaultIcon;
-      $groups[$key]['properties'] = [];
-    }
+    /**
+     * Get Global Content by key
+     *
+     * @param string $key
+     * @return string
+     */
+    public function get($key=NULL): string
+    {
+        if (!$key) return 'No key is set.';
 
-    // Create default group if not exist
-    if (!isset($groups[$defaultKey])) {
-      $groups[$defaultKey] = [
-        'label' => $defaultLabel,
-        'collapsed' => $defaultCollapsed,
-        'icon' => $defaultIcon,
-        'properties' => []
-      ];
-    }
+        /* build config filter string */
+        $instanceof = [];
+        foreach ($this->configuration['instanceof'] as $item) {
+            $instanceof [] = '[instanceof '.$item.']';
+        }
+        $instanceof = implode(',', $instanceof);
 
-    // Sort properties into groups
-    foreach($properties as $propKey => $propValue) {
-      // Skip if key is not a editable property
-      if (in_array($propKey, $skipProperties)) continue;
+        /* Find global content by key */
+        $context = $this->contextFactory->create(); //Erstellt einen Context mit den Standard parametern
+        $q = new FlowQuery([$context->getCurrentSiteNode()]);
+        $globalContent = $q->find($instanceof)->filter('['.$key.']')->property($key);
 
-      // Add missing properties to property
-      if (!isset($propValue['ui'])) $propValue['ui'] = ['group' => $defaultKey];
-      if (!isset($propValue['ui']['group'])) $propValue['ui']['group'] = $defaultKey;
-
-      // Append property to correct group
-      $groups[$propValue['ui']['group']]['properties'][$propKey] = $propValue;
-    }
-
-    return $groups;
+        /* Return */
+        return ($globalContent) ? $globalContent : 'Can\'t find global content with key: "' . $key . '"';
   }
 
-  /**
-   * All methods are considered safe, i.e. can be executed from within Eel
-   *
-   * @param string $methodName
-   * @return boolean
-   */
-  public function allowsCallOfMethod($methodName) {
-    return true;
-  }
+    /**
+     * Generate Global Content Array
+     *
+     * @param array $groups
+     * @param array $properties
+     * @return array
+     */
+    public function generateGlobalContentArray($groups, $properties): array
+    {
+        // Set group array if no group set
+        if (!isset($groups)) $groups = [];
+
+        // Init variables
+        $defaultKey = 'default';
+        $defaultLabel = 'General';
+        $defaultCollapsed = false;
+        $defaultIcon = 'file';
+        $skipProperties = ['metaRobotsNoindex', 'metaRobotsNofollow', '_hiddenInIndex'];
+
+        // Create group array and add properties
+        foreach ($groups as $key => $value) {
+            $group = $key;
+            if (!isset($value['collapsed'])) $groups[$key]['collapsed'] = $defaultCollapsed;
+            if (!isset($value['icon'])) $groups[$key]['icon'] = $defaultIcon;
+            $groups[$key]['properties'] = [];
+        }
+
+        // Create default group if not exist
+        if (!isset($groups[$defaultKey])) {
+            $groups[$defaultKey] = [
+                'label' => $defaultLabel,
+                'collapsed' => $defaultCollapsed,
+                'icon' => $defaultIcon,
+                'properties' => []
+            ];
+        }
+
+        // Sort properties into groups
+        foreach ($properties as $propKey => $propValue) {
+            // Skip if key is not a editable property
+            if (in_array($propKey, $skipProperties)) continue;
+
+            // Add missing properties to property
+            if (!isset($propValue['ui'])) $propValue['ui'] = ['group' => $defaultKey];
+            if (!isset($propValue['ui']['group'])) $propValue['ui']['group'] = $defaultKey;
+
+            // Append property to correct group
+            $groups[$propValue['ui']['group']]['properties'][$propKey] = $propValue;
+        }
+
+        return $groups;
+    }
+
+    /**
+     * All methods are considered safe, i.e. can be executed from within Eel
+     *
+     * @param string $methodName
+     * @return boolean
+     */
+    public function allowsCallOfMethod($methodName)
+    {
+        return true;
+    }
 }
